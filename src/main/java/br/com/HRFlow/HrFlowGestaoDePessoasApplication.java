dd8d4710717699a4cf59697c29408d6f88de5812
brunoverly <bsverly@gmai.com>
2026-04-20 17:24:15 -0300

    subindo o projeto para o gtihub

---
+++ src/main/java/br/com/HRFlow/HrFlowGestaoDePessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.HRFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrFlowGestaoDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrFlowGestaoDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/HRFlow/HrFlowGestaoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.HRFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrFlowGestaoDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
