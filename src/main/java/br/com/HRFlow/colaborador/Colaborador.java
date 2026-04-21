c6b8d37e7f0c85b9720ba5abba13497f6c9135e1
brunoverly <bsverly@gmai.com>
2026-04-20 21:20:13 -0300

    Add Colaborador

---
+++ src/main/java/br/com/HRFlow/colaborador/Colaborador.java
@@ -0,0 +1,35 @@
+package br.com.HRFlow.colaborador;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "colaboradores")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Colaborador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cargo;
+    private String departamento;
+    private LocalDateTime dataAdimissao;
+    private boolean ativo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Colaborador gestor;
+
+
+
+    @PrePersist
+    public void onCreate() {
+        this.ativo = true;
+    }
+
+}
